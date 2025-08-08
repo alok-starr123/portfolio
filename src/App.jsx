b35b9301032dabb9fc95763b711b02c580dc1959
alok-starr123 <as4692341@gmail.com>
2025-08-08 18:15:32 +0530

    Hero section with 3D model by gsap, framer and three.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
+import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
 
 const App = () => {
   return (
     <div className='relative w-screen min-h-screen overflow-x-hidden'>
       <Navbar />
+      <Hero />
+     
     </div>
   );
 };
---
+++ src/components/AnimatedTextLines.jsx
@@ -0,0 +1,38 @@
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+export const AnimatedTextLines = ({ text, className }) => {
+const lineRefs = useRef([]);
+const containerRef = useRef(null);
+const lines = text.split("\n").filter((line) => line.trim() !== "");
+     useGSAP(() => {
+        if (lineRefs.current.length > 0) {
+            gsap.from(lineRefs.current, {
+                y: 100,
+                opacity: 0,
+                duration: 1,
+                stagger: 0.3,
+                ease: "back.out",
+                scrollTrigger: {
+                    trigger: containerRef.current
+                },
+            });
+        }
+     });
+  
+    return (
+        <div ref={containerRef} className={className}>
+            {lines.map((line, index) => (
+                <span
+                 key={index}
+                 ref={(el) => (lineRefs.current[index] = el)}
+                  className="block leading-relaxed tracking-wide text-pretty">
+                    {line}
+                  </span>
+            ))}
+            </div>
+    );
+};
