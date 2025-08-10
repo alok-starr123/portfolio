bb3e6c3f7d973f9530e54fb755c119c1d34540f8
alok-starr123 <as4692341@gmail.com>
2025-08-10 15:09:05 +0530

    little update on animations in services sections with gsap

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,10 +1,29 @@
 import { useRef } from "react";
 import AnimatedHeaderSection from "../components/AnimatedHeaderSection";
 import { servicesData } from "../constants";
+import { useMediaQuery } from "react-responsive";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Services = () => {
     const text = `I build secure, high-perfomance full-stack web/apps with smoothUX to drive growth not headaches.`;
     const serviceRefs = useRef([]);
+    const isDekstop = useMediaQuery({ minWidth: "48rem" }); //768px
+    useGSAP(() => {
+        serviceRefs.current.forEach((el) => {
+            if(!el) return;
+
+            gsap.from(el, {
+                y: 200,
+                scrollTrigger: {
+                    trigger: el,
+                    start: "top 80%",
+                },
+                duration: 1,
+                ease: "circ.out",
+            });
+        });
+    }, []);
     return (
         <section id="services" className="min-h-screen bg-black rounded-t-4xl">
             <AnimatedHeaderSection subTitle={"Behind the scene, Beyond the screen"} title={"Service"} text={text} textColor={"text-white"}
@@ -12,7 +31,14 @@ const Services = () => {
             />
             {servicesData.map((service, index) => (
                 <div ref={(el) => (serviceRefs.current[index] = el )} key={index} className="sticky px-10 pt-6 text-white bg-black
-                border-t-2 border-white/30 top-0">
+                border-t-2 border-white/30"
+                style={isDekstop ? {
+                 top: `cal(10vh + ${index * 5})`,
+                 marginBottom: `${(servicesData.length - index - 1) * 5}rem`,
+                }
+                : { top: 0 }
+            }
+                >
                     <div className="flex items-center justify-between  gap-4 font-light">
                         <div className="flex flex-col gap-6">
                             <h2 className="text-4xl lg:text-5xl">{service.title}</h2>
