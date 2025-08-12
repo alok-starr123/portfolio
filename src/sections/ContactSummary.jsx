30b2bea8a41b139166803f819baa9ff5047656ac
alok-starr123 <as4692341@gmail.com>
2025-08-12 16:02:32 +0530

    import some fonts style

--- src/sections/ContactSummary.jsx
+++ src/sections/ContactSummary.jsx
@@ -26,7 +26,7 @@ const ContactSummary = () => {
             <div className="overflow-hidden font-light text-center contact-text-responsive">
                 <p>
                     " Let's build a <br />
-                    <span className="font-normal">memorable</span> & <span className="italic">inspiring</span>
+                    <span className="font-normal">memorable</span> &{" "} <span className="italic">inspiring</span>
                      <br />web application
                      <span className="text-gold"> together</span> "
                 </p>
